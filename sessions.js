e0a1be447f7387247aa556e11894db63815f59f8
maviperu <maviperu@gmail.com>
2025-09-16 21:21:15 -0400

    adding new sample session

--- sessions.js
+++ sessions.js
@@ -1,4 +1,5 @@
 export const recordedSessions = [
     {minsBeta:12,minsAlpha:11,minsTheta:13,seshType:"none"},
-    {minsBeta:22,minsAlpha:21,minsTheta:23,seshType:"none"}
+    {minsBeta:22,minsAlpha:21,minsTheta:23,seshType:"none"},
+    {deltaPower: 94, thetaPower: 41, alphaPower: 35, betaPower: 94, gammaPower: 72, sessionMinutes: 35, percentKept: 46, seshType: "none"},
 ]
