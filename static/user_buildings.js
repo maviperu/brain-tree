2585640c366f765a4c57ab569efd3ca198733b51
gnomeofdoom314 <gnomeofdoom314@gmail.com>
2025-10-10 15:25:48 -0400

    Autosave on menu Bugfix + progression framework

--- static/user_buildings.js
+++ static/user_buildings.js
@@ -1,9 +1,6 @@
 export const userBuildings = [
     {type:"House",wave:"1"},
-    {type:"Battery",wave:"Beta"},
     {type:"Mint",wave:"Alpha"},
     {type:"Mint",wave:"Beta"},
-    {type:"Battery",wave:"Alpha"},
-    {type:"Battery",wave:"Theta"},
     {type:"Mint",wave:"Theta"},
 ]
